959abc4887b57ac1723182dd45417babebe359a6
Krutagna Patel <kpatel.kp3101@gmail.com>
2024-03-21 11:30:44 +0530

    changes done

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,55 +1,60 @@
 import { IonIcon } from "@ionic/react";
-import { homeOutline, gridOutline, searchOutline, notificationsOutline, calculatorOutline, bookmarkOutline, settingsOutline, helpCircleOutline } from "ionicons/icons";
+import { logOutOutline, menuOutline } from "ionicons/icons";
+import navLinks from "../../data/data";
 import "./Sidebar.css";
+import { useState } from "react";
 
 function Sidebar() {
-  const navLinks = [
-    {
-      icon: homeOutline,
-      text: "Home",
-    },
-    {
-      icon: gridOutline,
-      text: "Dashboard",
-    },
-    {
-      icon: searchOutline,
-      text: "Dashboard",
-    },
-    {
-      icon: notificationsOutline,
-      text: "Notifications"
-    },
-    {
-      icon: calculatorOutline,
-      text: "Appointments"
-    },
-    {
-      icon: bookmarkOutline,
-      text: "Bookmarks"
-    },
-    {
-      icon: settingsOutline,
-      text: "Settings"
-    },
-    {
-      icon: helpCircleOutline,
-      text: "Help"
-    },
-  ]
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  function handleToggleSidebarIsOpen() {
+    setIsSidebarOpen((prevIsSidebarOpen) => !prevIsSidebarOpen);
+  }
+
+  console.log(isSidebarOpen);
 
   return (
-    <aside className="sidebar">
-      <nav className="sidebar__nav">
-        <ul className="sidebar__nav-list">
-          {navLinks.map((navLink, index) => (
-            <li className="sidebar__nav-link" key={index}>
-              <IonIcon className="sidebar__nav-icon" icon={navLink.icon} />
-              <span className="sidebar__nav-text">{navLink.text}</span>
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <aside
+      className={`sidebar flex flex-col justify-between gap ${
+        isSidebarOpen ? "sidebar--open" : ""
+      }`}
+    >
+      <div className="sidebar__content flow">
+        <div className="flex items-center justify-between gap">
+          {isSidebarOpen && (
+            <a href="#" className="sidebar__link sidebar__link--logo">
+              <h1>Coding</h1>
+            </a>
+          )}
+          <button
+            className="sidebar__toggle-btn grid place-content-center"
+            onClick={handleToggleSidebarIsOpen}
+          >
+            <IonIcon className="fs-400" icon={menuOutline} />
+          </button>
+        </div>
+        <nav className="sidebar__nav">
+          <ul className="sidebar__nav-list flex flex-col gap">
+            {navLinks.map((navLink, index) => (
+              <li key={index}>
+                <a className="sidebar__link sidebar__link--nav flex items-center gap" href="#">
+                  <IonIcon
+                    className="sidebar__nav-icon fs-300"
+                    icon={navLink.icon}
+                  />
+                  {isSidebarOpen && (
+                    <span className="sidebar__nav-text">{navLink.text}</span>
+                  )}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <a href="#" className="sidebar__link sidebar__link--logout flex items-center gap">
+        <IonIcon className="sidebar__logout-icon fs-300" icon={logOutOutline} />
+        {isSidebarOpen && <span className="sidebar__logout-text">Log out</span>}
+      </a>
     </aside>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,47 @@
+import {
+  homeOutline,
+  gridOutline,
+  searchOutline,
+  notificationsOutline,
+  calculatorOutline,
+  bookmarkOutline,
+  settingsOutline,
+  helpCircleOutline,
+} from "ionicons/icons";
+
+const navLinks = [
+  {
+    icon: homeOutline,
+    text: "Home",
+  },
+  {
+    icon: gridOutline,
+    text: "Dashboard",
+  },
+  {
+    icon: searchOutline,
+    text: "Search",
+  },
+  {
+    icon: notificationsOutline,
+    text: "Notifications",
+  },
+  {
+    icon: calculatorOutline,
+    text: "Appointments",
+  },
+  {
+    icon: bookmarkOutline,
+    text: "Bookmarks",
+  },
+  {
+    icon: settingsOutline,
+    text: "Settings",
+  },
+  {
+    icon: helpCircleOutline,
+    text: "Help",
+  },
+];
+
+export default navLinks;
